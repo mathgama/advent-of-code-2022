68c128dfb7329aa64d50caf39e422f22438e2984
Matheus Gama <matheus_gama@live.com>
2022-12-04 21:10:27 -0300

    day01 - part 2

--- src/day01/day01.js
+++ src/day01/day01.js
@@ -2,7 +2,7 @@ import { readInput } from '../utils.js'
 
 const input = readInput('./src/day01/input.txt')
 
-const partOne = () => {
+const sumCaloriesByElf = () => {
   let elfIndex = 0
   const elfCalories = [].fill(0)
 
@@ -13,8 +13,29 @@ const partOne = () => {
     elfCalories[elfIndex] += +foodCalories
   })
 
+  return elfCalories
+}
+
+const partOne = () => {
+  const elfCalories = sumCaloriesByElf()
+
   console.log('-- Part One --')
   console.log('Result: ', Math.max(...elfCalories))
 }
 
+const partTwo = () => {
+  let result = 0
+  let elfCalories = sumCaloriesByElf()
+
+  elfCalories = elfCalories.sort((a, b) => b - a)
+
+  for (let i = 0; i < 3; i += 1) {
+    result += elfCalories[i]
+  }
+
+  console.log('-- Part Two --')
+  console.log('Result: ', result)
+}
+
 partOne()
+partTwo()
